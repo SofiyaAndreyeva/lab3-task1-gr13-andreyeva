7a460c221aedc3d7bcbc44ff55c449762d35d2c2
SofiyaAndreyeva <137882389+SofiyaAndreyeva@users.noreply.github.com>
2024-02-28 16:27:23 +0000

    add count.c and .h

--- src/main.c
+++ src/main.c
@@ -1,18 +1,4 @@
-#include <stdio.h>
-
-int countOccurrences(char *str, char letter) {
-
-
-  int count = 0;
-  while (*str != '\0') { // пока нет конца строки
-    if (letter == *str) { //если буква равна нужной букве
-      count++;
-    }
-    str++;
-  }
-  return count;
-}
-
+#include "count.c"
 int main() {
   char sentence[1000];
   char letter;
