d32131ae740f361969335a55c3c43c281d527048
SofiyaAndreyeva <137882389+SofiyaAndreyeva@users.noreply.github.com>
2024-02-28 09:51:59 +0000

    add printf, scanf  and announced function

--- .ccls-cache/@home@runner@lab3-task1-gr13-andreyeva/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr13-andreyeva/src@main.c
@@ -1,8 +1,33 @@
-/* hello.c */
+
 #include <stdio.h>
 
-void main (void)
+//функция подсчета вхождения буквы в предложении
+int countOccurrences(char *str, char letter) {
+
+}
+
+int main ()
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  char sentence[1000];
+  char letter;
+
+  //ввод предложения 
+  printf("Введите предложение: ");
+  fgets(sentence, sizeof(sentence), stdin); 
+
+  //ввод искаемой буквы
+  printf("Введите букву для поиска: ");
+  scanf("%c", &letter); 
+
+  // подсчет количества 
+  int occurrences = countOccurrences(sentence, letter); 
+
+  //вывод результата 
+  if (occurrences == 0) {
+    printf("Буквы '%c' нет в предложении.\n",letter);
+  }else {
+    printf("Буква '%c' встречается %d раз(а) в предложении.\n", letter, occurrences);
+  }
+
+  return 0;
 }
