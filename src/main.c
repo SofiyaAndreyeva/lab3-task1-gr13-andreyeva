17035855383cd8fd2fbb7385486ee8acd8dd4862
SofiyaAndreyeva <137882389+SofiyaAndreyeva@users.noreply.github.com>
2024-02-28 15:32:43 +0000

    add function

--- src/main.c
+++ src/main.c
@@ -4,6 +4,14 @@
 //функция подсчета вхождения буквы в предложении
 int countOccurrences(char *str, char letter) {
 
+  int count = 0;
+  while(*str != '\0'){ // пока нет конца строки
+    if (letter == *str){ //если буква равна нужной букве
+      count++;
+    }
+    str++;
+  }
+  return count;
 }
 
 int main ()
